9180b29e14bb6ac7e3b08b2c679d368a6b266e3f
Alexander Bogish <ego.cogitare@gmail.com>
2018-01-04 00:44:48 +0200

    ...

--- src/js/app.js
+++ src/js/app.js
@@ -17,40 +17,43 @@ $(document).ready(function() {
     speed: 1000
   });
 
-  var brandSlider = new Swiper('#brands-slider', {
-    direction: 'horizontal',
-    slidesPerView: 6,
-    nextButton: $(this).find('.btn-next'),
-    prevButton: $(this).find('.btn-prev'),
-    paginationClickable: true,
-    loop: false,
-    spaceBetween: 4,
-    mousewheelControl: false,
-    speed: 1000,
-    autoResize: false,
-    resizeReInit: true,
-    onSlideChangeStart: function(swiper) {
-      $(this).find('.slider-arrow').removeClass('hide');
-      if (swiper.isBeginning) {
-        $(this).find('.btn-prev').addClass('hide');
-      }
-      if (swiper.isEnd) {
-        $(this).find('.btn-next').addClass('hide');
-      }
-    }.bind(this)
-  });
+  if ($('#brands-slider').length !== 0)
+  {
+    var brandSlider = new Swiper('#brands-slider', {
+      direction: 'horizontal',
+      slidesPerView: 6,
+      nextButton: $(this).find('.btn-next'),
+      prevButton: $(this).find('.btn-prev'),
+      paginationClickable: true,
+      loop: false,
+      spaceBetween: 4,
+      mousewheelControl: false,
+      speed: 1000,
+      autoResize: false,
+      resizeReInit: true,
+      onSlideChangeStart: function(swiper) {
+        $(this).find('.slider-arrow').removeClass('hide');
+        if (swiper.isBeginning) {
+          $(this).find('.btn-prev').addClass('hide');
+        }
+        if (swiper.isEnd) {
+          $(this).find('.btn-next').addClass('hide');
+        }
+      }.bind(this)
+    });
 
-  // $(window).bind('resize', function() {
-  //   var width = $('body').width();
-  //
-  //   if (width >= 1680)                      brandSlider.params.slidesPerView = 6;
-  //   else if (width >= 1367 && width < 1680) brandSlider.params.slidesPerView = 5;
-  //   else if (width > 1024 && width < 1367)  brandSlider.params.slidesPerView = 4;
-  //   else if (width >= 768 && width <= 1024) brandSlider.params.slidesPerView = 3;
-  //   else                                    brandSlider.params.slidesPerView = 1;
-  //   brandSlider.update();
-  // });
-  // $(window).trigger('resize');
+    $(window).bind('resize', function() {
+      var width = $('body').width();
+
+      if (width >= 1680)                      brandSlider.params.slidesPerView = 6;
+      else if (width >= 1367 && width < 1680) brandSlider.params.slidesPerView = 5;
+      else if (width > 1024 && width < 1367)  brandSlider.params.slidesPerView = 4;
+      else if (width >= 768 && width <= 1024) brandSlider.params.slidesPerView = 3;
+      else                                    brandSlider.params.slidesPerView = 1;
+      brandSlider.update();
+    });
+    $(window).trigger('resize');
+  }
 
   $('#price-range').jRange({
     from: 0,
